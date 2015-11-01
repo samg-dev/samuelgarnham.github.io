d1113df51ba59631dfc0edc7a85c74b5859ca1ce
Samuel Garnham <1120118@my.brunel.ac.uk>
2015-11-01 14:03:59 -0500

    Rendering into body now

---
+++ components/Main.js
@@ -0,0 +1,42 @@
+/** @jsx React.DOM */
+
+var Main = React.createClass({
+
+  render: function() {
+    
+    return (
+
+      <div id="header">
+        <nav>
+          <li class="fork"><a href="https://github.com/samuelgarnham">View On GitHub</a></li>
+        </nav>
+      </div><!-- end header -->
+
+    <div class="wrapper">
+
+      <section>
+        <div id="title">
+          <h1>Samuelgarnham.GitHub.io</h1>
+          <p>My GitHub Pages home page</p>
+          <hr>
+          <span class="credits left">Project maintained by <a href="https://github.com/samuelgarnham">samuelgarnham</a></span>
+          <span class="credits right">Hosted on GitHub Pages &mdash; Theme by <a href="https://twitter.com/michigangraham">mattgraham</a></span>
+        </div>
+	<div>
+	  <TimerExample start={Date.now()} >
+        </div>
+      </section>
+
+    </div>
+    <!--[if !IE]><script>fixScale(document);</script><![endif]--> 
+
+    )
+
+  }
+});
+
+
+React.render(
+    <Main />,
+    document.body
+);
