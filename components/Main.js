9056eebb7f92d93781c0b1f2fb27a3b63546d403
Samuel Garnham <1120118@my.brunel.ac.uk>
2015-11-01 14:17:04 -0500

    removed <hr> tag

--- components/Main.js
+++ components/Main.js
@@ -18,12 +18,8 @@ var Main = React.createClass({
         <div id="title">
           <h1>Samuelgarnham.GitHub.io</h1>
           <p>My GitHub Pages home page</p>
-          <hr>
           <span class="credits left">Project maintained by <a href="https://github.com/samuelgarnham">samuelgarnham</a></span>
           <span class="credits right">Hosted on GitHub Pages &mdash; Theme by <a href="https://twitter.com/michigangraham">mattgraham</a></span>
-        </div>
-	<div>
-	  <TimerExample start={Date.now()} >
         </div>
       </section>
 
