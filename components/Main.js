af746b42bdb5e4bf0c9836ef9d93bbe10f14d4b4
Samuel Garnham <1120118@my.brunel.ac.uk>
2015-11-03 11:02:28 -0500

    Now?

--- components/Main.js
+++ components/Main.js
@@ -7,29 +7,8 @@ var Main = React.createClass({
     return (
       <div>
         Hello
-//      <div id="header">
-//        <nav>
-//          <li class="fork"><a href="https://github.com/samuelgarnham">View On GitHub</a></li>
-//        </nav>
-//      </div>
       </div>
-//    <div class="wrapper">
-//
-//      <section>
-//        <div id="title">
-//          <h1>Samuelgarnham.GitHub.io</h1>
-//          <p>My GitHub Pages home page</p>
-//          <hr>
-//          <span class="credits left">Project maintained by <a href="https://github.com/samuelgarnham">samuelgarnham</a></span>
-//          <span class="credits right">Hosted on GitHub Pages &mdash; Theme by <a href="https://twitter.com/michigangraham">mattgraham</a></span>
-//        </div>
-//      </section>
-//
-//    </div>
-//    <!--[if !IE]><script>fixScale(document);</script><![endif]--> 
-//    </div>
     )
-
   }
 });
 
