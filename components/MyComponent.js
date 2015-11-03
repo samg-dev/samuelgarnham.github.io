cc2de00b3a5d8f7178bd9b7480708891e3cd4b95
Samuel Garnham <1120118@my.brunel.ac.uk>
2015-11-03 16:19:54 -0500

    MyComponent.js added

---
+++ components/MyComponent.js
@@ -0,0 +1,12 @@
+var MyComponent = React.createClass({
+    render: function(){
+        return (
+            <h1>Hello, world!</h1>
+        );
+    }
+});
+
+React.render(
+  <MyComponent />,
+  document.getElementById('content')
+);
